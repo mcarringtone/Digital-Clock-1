647707de67cfb34bb7b4b2a6715110e2b54ba51d
Carrington <55025157+mcarringtone@users.noreply.github.com>
2021-09-07 18:10:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 
 function clockApp(){
 
-  const fullDate = new Date();
-  let hours = fullDate.getHours();
-  let minutes = fullDate.getMinutes();
-  let seconds = fullDate.getSeconds();
+let fullDate = new Date();
+let hours = fullDate.getHours();
+let minutes = fullDate.getMinutes();
+let seconds = fullDate.getSeconds();
 
-  if(hours < 10){
-    hours = "0" + hours
-  }
-  if(minutes < 10){
-    minutes = "0" + minutes
-  }
-  if(seconds < 10){
-    seconds = "0" + seconds
-  }
-  if(hours > 12){
-    hours = hours - 12
-    hours = "0" + hours
-  }
-  document.getElementById("Hours").innerHTML = hours + ":"
-  document.getElementById("Minutes").innerHTML = minutes + ":"
-  document.getElementById("Seconds").innerHTML = seconds 
+if(hours < 10){
+  hours = "0" + hours ;
+}
+if(minutes < 10){
+  minutes = "0" + minutes;
+}
+if(seconds < 10){
+  seconds = "0" + seconds
+}
+if(hours > 13){
+  hours = hours - 12;
+  hours = "0" + hours ;
 }
 
-
-
+document.getElementById("hour").innerHTML = hours + " :";
+document.getElementById("minutes").innerHTML = minutes + " :";
+document.getElementById("seconds").innerHTML = seconds;
+}
 
 setInterval(clockApp,1000)
-
-
